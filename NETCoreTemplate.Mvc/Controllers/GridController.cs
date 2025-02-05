12ab31ed750a3dc72f6420298537d06065310b97
Jamfin92 <36896551+Jamfin92@users.noreply.github.com>
2025-02-05 06:00:24 -0500

    more generated changes for gridcontroller

---
+++ NETCoreTemplate.Mvc/Controllers/GridController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NETCoreTemplate.Mvc.Controllers
+{
+    public class GridController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
